67dbbe033ec1e303b51f8cdbdc7163968e0b44df
Adrian <Adrian@DESKTOP-75CH9HE>
2023-11-10 10:32:25 +0100

    VECTORES Y CADENA
    FUNCIONES DE TODOS LOS EJERCICIOS QUE SE HAN REALIZADO PARA COMPLETAR LA
    RELACION DE EJERCICIOS DE VECOTRES Y CADENAS

--- VECTORES_Y_CADENAS/funciones.c
+++ VECTORES_Y_CADENAS/funciones.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 #include <time.h>
 
 void EJ1(int array[LONG_ARR], int longitud)
@@ -60,7 +61,7 @@ int esNif(char cadena[])
 	{
 		valor = atoi(&cadena[i]);
 
-		printf("\nPosicion %i: valor de %c: %i", i+1, cadena[i], valor);
+//		printf("\nPosicion %i: valor de %c: %i", i+1, cadena[i], valor);
 		if (valor == 0 && cadena[i] != '0') {salida = 0; break;}
 		else if (cadena[i] < '0' || cadena[i] > '9') {salida = 0; break;}
 
@@ -75,4 +76,106 @@ int esNif(char cadena[])
 
 
 	return salida;
+}
+
+void DNI (char cadena[])
+{
+	int i;
+	for (i = 0; i < 8; i++)
+	{
+		printf("%c", cadena[i]);
+	}
+}
+
+int esCapicua(char cadena[])
+{
+	int i;
+	int salida = 1;
+	int rango = strlen(cadena) -1;
+	for (i = 0; i <= rango /2; i++)
+	{
+		if (cadena[i] != cadena[rango - i]) { salida = 0; break;}
+	}
+
+	return salida;
+}
+
+void EJ4(float array[], int rango, int posicionCambio, float numCambio) {
+    int i;
+
+    for (i = rango - 1; i > posicionCambio; i--) {
+        array[i] = array[i - 1];
+    }
+
+    array[posicionCambio] = numCambio;
+}
+
+int calculoMayor (int array[], int rango)
+{
+	int i;
+	int mayor, compM;
+	for (i = 0; i < rango; i++)
+	{
+		if (i == 0) { mayor = i; compM = array[i];}
+		else {
+			if (array[i] > compM) {mayor = i; compM = array[i];}
+		}
+	}
+
+	return mayor;
+}
+
+int calculoMenor (int array[], int rango)
+{
+	int i;
+	int menor, compm;
+	for (i = 0; i < rango; i++)
+	{
+		if (i == 0) { menor = i; compm = array[i];}
+		else {
+			if (array[i] < compm) {menor = i; compm = array[i];}
+		}
+	}
+
+	return menor;
+}
+
+float calculoMedia (int array[], int rango)
+{
+	int i;
+	float acumulador = 0;
+
+	for (i = 0; i < rango; i++)
+	{
+		acumulador += (float) array[i];
+	}
+
+	return acumulador / (float)rango;
+}
+
+float calculoDesTipica(int array[], int rango)
+{
+	float media = calculoMedia(array, rango);
+	float acumulador = 0;
+	int i;
+	for (i = 0; i < rango; i++)
+	{
+		acumulador += (pow((float) array[i] - media, 2));
+	}
+
+	return sqrt(acumulador / rango);
+}
+
+void EJ6(int num, int array[], int *cont)
+{
+	int cociente, resto;
+
+	do {
+		resto = num %10;
+		cociente = num / 10;
+		 array[*cont] = resto;
+		 *cont = *cont + 1;
+		 num = cociente;
+
+	} while (cociente != 0);
 }
--- VECTORES_Y_CADENAS/funciones.h
+++ VECTORES_Y_CADENAS/funciones.h
@@ -14,4 +14,13 @@ void EJ1(int array[LONG_ARR], int longitud);
 int genNumAl (int num);
 void invertir (char array[LONG_ARR], int longitud);
 int esNif(char cadena[]);
+void DNI (char cadena[]);
+int esCapicua(char cadena[]);
+void EJ4(float array[], int rango, int posicionCambio, float numCambio);
+int calculoMayor (int array[], int rango);
+int calculoMenor (int array[], int rango);
+float calculoMedia(int array[], int rango);
+float calculoDesTipica(int array[], int rango);
+void EJ6(int num, int array[], int *cont);
+
 #endif /* FUNCIONES_H_ */
